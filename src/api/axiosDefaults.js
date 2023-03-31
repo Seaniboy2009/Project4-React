06e3955891c97a0aab5cd1524c2dfbea83874d0a
Sean Clark <seaniboy2009@yahoo.co.uk>
2023-03-31 12:35:39 +0000

    add error handeling for users o see when creating account

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://project-5-api.herokuapp.com/'
-axios.defaults.headers.post['Content-Type'] = 'muiltpart/form-data'
-axios.defaults.withCredentials = true
+axios.defaults.baseURL = 'https://project-5-api.herokuapp.com/';
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
+axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,19 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import btnstyles from '../../styles/Button.module.css'
-import { Row, Col, Form, Button } from 'react-bootstrap'
+import { Row, Col, Form, Button, Alert } from 'react-bootstrap'
+import axios from "axios";
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
 
 const SignUpForm = () => {
+    const [formData, setFormData] = useState({username: '', password1: '', password2: ''});
+    const {username, password1, password2} = formData;
+    const [errors, setErrors] = useState({});
+    const history = useHistory();
+
+    const handleChange = (event) => {
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value
+        });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            await axios.post("https://project-5-api.herokuapp.com/dj-rest-auth/registration/", formData);
+            history.push("/signin");
+        } catch (errors) {
+            console.log(errors.response?.data)
+            setErrors(errors.response?.data)
+        }
+    }
+
     return (
         <Row className="justify-content-md-center">
             <Col xs={6}>
-                <Form>
-                    <Form.Group controlId="name">
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group controlId="username">
                         <Form.Label>Name</Form.Label>
                         <Form.Control
                             type="text"
                             placeholder="Enter Name"
-                            name='name'
+                            name='username'
+                            value={username}
+                            onChange={handleChange}
                         />
+                        {errors.name?.map((message, index) => 
+                            <Alert key={index}>{message}</Alert>
+                        )}
                     </Form.Group>
                     <Form.Group controlId="password1">
                         <Form.Label>Password</Form.Label>
@@ -21,7 +51,12 @@ const SignUpForm = () => {
                             type="password"
                             placeholder="Password"
                             name='password1'
+                            value={password1}
+                            onChange={handleChange}
                         />
+                        {errors.password1?.map((message, index) => 
+                            <Alert key={index}>{message}</Alert>
+                        )}
                     </Form.Group>
                     <Form.Group controlId="password2">
                         <Form.Label>Confirm password</Form.Label>
@@ -29,11 +64,19 @@ const SignUpForm = () => {
                             type="password"
                             placeholder="Confirm password"
                             name='password2'
+                            value={password2}
+                            onChange={handleChange}
                         />
+                        {errors.password2?.map((message, index) => 
+                            <Alert key={index}>{message}</Alert>
+                        )}
                     </Form.Group>
                     <Button className={`${btnstyles.Full} ${btnstyles.Main}`} type="submit">
                         Sign up
                     </Button>
+                    {errors.non_fields_errors?.map((message, index) => 
+                            <Alert key={index}>{message}</Alert>
+                        )}
                 </Form>
             </Col>
         </Row>
