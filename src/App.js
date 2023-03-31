952d428d844a33161a4a994aa5b3e5d229e4fc2d
Sean Clark <seaniboy2009@yahoo.co.uk>
2023-03-31 09:10:49 +0000

    add routing, add styles for navbar.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import styles from './App.module.css';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
+import { Container } from 'react-bootstrap';
+import {Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
-      <h1>Hello World</h1>
+      <Container className={styles.Main}>
+        <Switch >
+          <Route exact path='/' render={() => <h1>Hello World</h1>} />
+          <Route exact path='/signin' render={() => <h1>Sign in</h1>} />
+          <Route exact path='/signup' render={() => <h1>Sign up</h1>} />
+          <Route exact path='/signout' render={() => <h1>Sign out</h1>} />
+        </Switch>
+      </Container>
       <Footer />
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,19 @@
 import React from 'react'
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import styles from '../styles/NavBar.module.css'
+import { Navbar, Nav } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
     return (
-        <Navbar bg="light" expand="lg" fixed='top'>
+        <Navbar className={styles.NavBar} bg="light" expand="lg" fixed='top'>
             <Navbar.Brand href="#home">As Advertised</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto text-left">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <NavDropdown title="Account" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">My posts</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Log out</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Account settings</NavDropdown.Item>
-                    </NavDropdown>
-                    <Nav.Link href="#home">Liked</Nav.Link>
-                    <Nav.Link href="#home">Followed</Nav.Link>
+                    <NavLink className={styles.Link} to='/'>Home</NavLink>
+                    <NavLink className={styles.Link} to='/signin'>Sign in</NavLink>
+                    <NavLink className={styles.Link} to='/signup'>Sign up</NavLink>
+                    <NavLink className={styles.Link} to='/signout'>Sign out</NavLink>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
