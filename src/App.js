46bb712c9e17c6fe2b1264e4a22954a474aa2eda
Sean Clark <seaniboy2009@yahoo.co.uk>
2023-03-31 13:51:42 +0000

    add signin page and styles

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavBar from './components/NavBar';
 import { Container } from 'react-bootstrap';
 import {Route, Switch} from 'react-router-dom'
 import SignUpForm from './pages/auth/SignUpForm';
+import SignInForm from './pages/auth/SignInForm';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
       <Container className={styles.Main}>
         <Switch >
           <Route exact path='/' render={() => <h1>Hello World</h1>} />
-          <Route exact path='/signin' render={() => <h1>Sign in</h1>} />
+          <Route exact path='/signin' render={() => <SignInForm />} />
           <Route exact path='/signup' render={() => <SignUpForm />} />
           <Route exact path='/signout' render={() => <h1>Sign out</h1>} />
           <Route render={() => <h1>Page not found</h1>} />
--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -4,30 +4,31 @@ import { Row, Col, Form, Button, Alert } from 'react-bootstrap'
 import axios from "axios";
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
 
-const SignUpForm = () => {
-    const [formData, setFormData] = useState({username: '', password1: '', password2: ''});
-    const {username, password1, password2} = formData;
+const SignInForm = () => {
+    const [formData, setFormData] = useState({
+        username: '',
+        password: '',
+    });
+    const {username, password} = formData;
     const [errors, setErrors] = useState({});
     const history = useHistory();
 
     const handleChange = (event) => {
         setFormData({
             ...formData,
-            [event.target.name]: event.target.value
-        });
-    };
-
+            [event.target.name]: event.target.value,
+        })
+    }
     const handleSubmit = async (event) => {
         event.preventDefault();
         try {
-            await axios.post("https://project-5-api.herokuapp.com/dj-rest-auth/registration/", formData);
+            await axios.post('https://project-5-api.herokuapp.com/dj-rest-auth/login/', formData)
             history.push("/");
         } catch (errors) {
-            console.log(errors.response?.data)
             setErrors(errors.response?.data)
+            console.log(errors.response)
         }
     }
-
     return (
         <Row className="justify-content-md-center">
             <Col xs={6}>
@@ -41,46 +42,33 @@ const SignUpForm = () => {
                             value={username}
                             onChange={handleChange}
                         />
-                        {errors.name?.map((message, index) => 
+                        {errors.username?.map((message, index) =>
                             <Alert key={index}>{message}</Alert>
                         )}
                     </Form.Group>
-                    <Form.Group controlId="password1">
+                    <Form.Group controlId="password">
                         <Form.Label>Password</Form.Label>
                         <Form.Control
                             type="password"
                             placeholder="Password"
-                            name='password1'
-                            value={password1}
+                            name='password'
+                            value={password}
                             onChange={handleChange}
                         />
-                        {errors.password1?.map((message, index) => 
-                            <Alert key={index}>{message}</Alert>
-                        )}
-                    </Form.Group>
-                    <Form.Group controlId="password2">
-                        <Form.Label>Confirm password</Form.Label>
-                        <Form.Control
-                            type="password"
-                            placeholder="Confirm password"
-                            name='password2'
-                            value={password2}
-                            onChange={handleChange}
-                        />
-                        {errors.password2?.map((message, index) => 
+                        {errors.password?.map((message, index) =>
                             <Alert key={index}>{message}</Alert>
                         )}
                     </Form.Group>
                     <Button className={`${btnstyles.Full} ${btnstyles.Main}`} type="submit">
-                        Sign up
+                        Sign in
                     </Button>
-                    {errors.non_field_errors?.map((message, index) => 
-                            <Alert key={index}>{message}</Alert>
-                        )}
+                    {errors.non_field_errors?.map((message, index) =>
+                        <Alert key={index}>{message}</Alert>
+                    )}
                 </Form>
             </Col>
         </Row>
     )
 }
 
-export default SignUpForm
+export default SignInForm
