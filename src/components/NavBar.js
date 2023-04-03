a2fedf7ab1c300967b834a42686576fe475cd921
Sean Clark <seaniboy2009@yahoo.co.uk>
2023-04-03 10:13:24 +0000

    refactor navbar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -19,14 +19,30 @@ const NavBar = () => {
                     {signedInUser ?
                         (
                             <>
-                            <NavLink className={styles.Link} activeClassName={styles.Active} to='/signout'><i className="fa-solid fa-arrow-right-from-bracket"></i> Sign out</NavLink>
-                            <NavLink className={styles.Link} activeClassName={styles.Active} to='/account'><i className="fa-solid fa-user"></i> Account: {signedInUser.username}</NavLink>
+                                <NavLink
+                                    className={styles.Link}
+                                    activeClassName={styles.Active} to='/signout'>
+                                    <i className="fa-solid fa-arrow-right-from-bracket"></i> Sign out
+                                </NavLink>
+                                <NavLink
+                                    className={styles.Link}
+                                    activeClassName={styles.Active} to='/account'>
+                                    <i className="fa-solid fa-user"></i> Account: {signedInUser.username}
+                                </NavLink>
                             </>
                         ) :
                         (
                             <>
-                            <NavLink className={styles.Link} activeClassName={styles.Active} to='/signup'><i className="fa-solid fa-user-plus"></i> Sign up</NavLink>
-                            <NavLink className={styles.Link} activeClassName={styles.Active} to='/signin'><i className="fa-solid fa-arrow-right-to-bracket"></i> Sign in</NavLink>
+                                <NavLink
+                                    className={styles.Link}
+                                    activeClassName={styles.Active} to='/signup'>
+                                    <i className="fa-solid fa-user-plus"></i> Sign up
+                                </NavLink>
+                                <NavLink
+                                    className={styles.Link}
+                                    activeClassName={styles.Active} to='/signin'>
+                                    <i className="fa-solid fa-arrow-right-to-bracket"></i> Sign in
+                                </NavLink>
                             </>
                         )}
                 </Nav>
