2934d950e138dad569f337d05be4d65368ff51e4
Sean Clark <seaniboy2009@yahoo.co.uk>
2023-04-03 17:55:57 +0000

    add form control for postcreateform.js

--- src/components/Asset.js
+++ src/components/Asset.js
@@ -1,26 +1,66 @@
 
 import React from "react";
-import { Container, Spinner } from "react-bootstrap";
+import { Container, Spinner, Row, Col } from "react-bootstrap";
 import styles from "../styles/Asset.module.css";
 
-const Asset = ({ spinner, srcAlike, srcNotAlike, message, messageAlike, messageNotAlike }) => {
+// const Asset = ({ spinner, srcAlike, srcNotAlike, message, messageAlike, messageNotAlike }) => {
+//     return (
+//         <div className={`${styles.Asset}`}>
+//             {spinner && <Spinner animation="border" />}
+//             {
+//                 <>
+//                     <Container>
+//                         <Row>
+//                             <Col>
+//                                 {messageAlike &&
+//                                     <p className={`${styles.Alike} mt-4`}>
+//                                         {messageAlike}
+//                                     </p>
+//                                 }
+//                                 {srcAlike &&
+//                                     <img className={styles.AlikeImage}
+//                                         src={srcAlike}
+//                                         alt={messageAlike}
+//                                         width={200}
+//                                     />
+//                                 }
+//                             </Col>
+//                             <Col>
+//                                 {messageNotAlike &&
+//                                     <p className={`${styles.NotAlike} mt-4`}>
+//                                         {messageNotAlike}
+//                                     </p>
+//                                 }
+//                                 {srcNotAlike &&
+//                                     <img
+//                                         className={styles.NotAlikeImage}
+//                                         src={srcNotAlike}
+//                                         alt={messageNotAlike}
+//                                         width={200}
+//                                     />
+//                                 }
+//                             </Col>
+//                         </Row>
+//                         <Row>
+//                             <Col>
+//                                 <p>{message}</p>
+//                             </Col>
+//                         </Row>
+//                     </Container>
+//                 </>
+//             }
+//         </div>
+//     );
+// };
+
+const Asset = ({ spinner, src, message }) => {
     return (
-        <div className={`${styles.Asset}`}>
-            {spinner && <Spinner animation="border" />}
-            {
-                <>
-                    <Container>
-                        {srcAlike && <img className={styles.AlikeImage} src={srcAlike} alt={messageAlike} width={200} />}
-                        {srcNotAlike && <img className={styles.NotAlikeImage} src={srcNotAlike} alt={messageNotAlike} width={200} />}
-                    </Container>
-                    <Container>
-                        {messageAlike && <p className={`${styles.Alike} mt-4`}>{messageAlike}</p>}
-                        {messageNotAlike && <p className={`${styles.NotAlike} mt-4`}>{messageNotAlike}</p>}
-                    </Container>
-                </>
-            }
-        </div>
+      <div className={`${styles.Asset} p-4`}>
+        {spinner && <Spinner animation="border" />}
+        {src && <img src={src} alt={message} />}
+        {message && <p className="mt-4">{message}</p>}
+      </div>
     );
-};
+  };
 
 export default Asset;
--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -1,16 +1,97 @@
 import React, { useState } from "react";
-import { Button, Form, Row, Col, Container } from "react-bootstrap";
+import { Button, Form, Row, Col, Container, Alert, Image } from "react-bootstrap";
 import appStyles from "../../App.module.css";
 import Asset from "../../components/Asset";
 import btnStyles from "../../styles/Button.module.css";
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
 
 function PostCreateForm() {
 
-    const [errors, setErrors] = useState({});
     const postURL = 'https://res.cloudinary.com/dgj9rjuka/image/upload/v1678359959/media/images/default_post_fr07hq.jpg'
+
+    // Data that will be sent to the API
+    const [formData, setFormData] = useState({
+        title: '',
+        location: '',
+        franchisor: '',
+        content: '',
+        advert_image: '',
+        reality_image: '',
+    });
+    const { title, location, franchisor, content, advert_image, reality_image } = formData;
+    const [errors, setErrors] = useState({});
+    const history = useHistory();
+
+    // Handle the changes made on the form
+    const handleChange = (event) => {
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value,
+        })
+    }
+
+    const handleChangeImageAdvert = (event) => {
+        if (event.target.files.length) {
+            URL.revokeObjectURL(advert_image);
+            setFormData({
+                ...formData,
+                advert_image: URL.createObjectURL(event.target.files[0]),
+            });
+        }
+    };
+
+    const handleChangeImageReality = (event) => {
+        if (event.target.files.length) {
+            URL.revokeObjectURL(reality_image);
+            setFormData({
+                ...formData,
+                reality_image: URL.createObjectURL(event.target.files[0]),
+            });
+        }
+    };
 
     const textFields = (
         <div className="text-center">
+            <Form.Group controlId="title">
+                <Form.Label>Post Title</Form.Label>
+                <Form.Control
+                    type="text"
+                    placeholder="Enter Title"
+                    name='title'
+                    value={title}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+            <Form.Group controlId="location">
+                <Form.Label>Post Title</Form.Label>
+                <Form.Control
+                    type="text"
+                    placeholder="Location"
+                    name='location'
+                    value={location}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+            <Form.Group controlId="franchisor">
+                <Form.Label>Post Title</Form.Label>
+                <Form.Control
+                    type="text"
+                    placeholder="Franchise"
+                    name='franchisor'
+                    value={franchisor}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+            <Form.Group controlId="content">
+                <Form.Label>Content</Form.Label>
+                <Form.Control
+                    type="text"
+                    placeholder="Content"
+                    name='content'
+                    value={content}
+                    onChange={handleChange}
+                />
+            </Form.Group>
             <Button
                 className={`${btnStyles.Full} ${btnStyles.Main}`}
                 onClick={() => { }}
@@ -27,23 +108,72 @@ function PostCreateForm() {
         <Form>
             <Row>
                 <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
-                    <Container
-                        className={`${appStyles.Container} d-flex flex-column justify-content-center`}
-                    >
+                    <Container className={`${appStyles.Container} d-flex flex-column justify-content-center`}>
                         <Form.Group className="text-center">
+                            {advert_image ? (
+                                <>
+                                    <figure>
+                                        <Image className={appStyles.Image} src={advert_image} rounded />
+                                    </figure>
+                                    <div>
+                                        <Form.Label
+                                            className={`${btnStyles.Button} ${btnStyles.Blue} btn`}
+                                            htmlFor="image-upload-advert"
+                                        >
+                                            Change the image
+                                        </Form.Label>
+                                    </div>
+                                </>
+                            ) : (
+                                <Form.Label
+                                    className="d-flex justify-content-center"
+                                    htmlFor="image-upload-advert"
+                                >
+                                    <Asset
+                                        src={postURL}
+                                        message="Click or tap to upload an image"
+                                    />
+                                </Form.Label>
+                            )}
 
-                            <Form.Label
-                                className="d-flex justify-content-center"
-                                htmlFor="image-upload"
-                            >
-                                <Asset
-                                    srcAlike={postURL}
-                                    srcNotAlike={postURL}
-                                    messageAlike='Advert image'
-                                    messageNotAlike='Actual product'
-                                />
-                            </Form.Label>
+                            <Form.File
+                                id="image-upload-advert"
+                                accept="image/*"
+                                onChange={handleChangeImageAdvert}
+                            />
+                        </Form.Group>
+                        <Form.Group className="text-center">
+                            {reality_image ? (
+                                <>
+                                    <figure>
+                                        <Image className={appStyles.Image} src={reality_image} rounded />
+                                    </figure>
+                                    <div>
+                                        <Form.Label
+                                            className={`${btnStyles.Button} ${btnStyles.Blue} btn`}
+                                            htmlFor="image-upload-reality"
+                                        >
+                                            Change the image
+                                        </Form.Label>
+                                    </div>
+                                </>
+                            ) : (
+                                <Form.Label
+                                    className="d-flex justify-content-center"
+                                    htmlFor="image-upload-reality"
+                                >
+                                    <Asset
+                                        src={postURL}
+                                        message="Click or tap to upload an image"
+                                    />
+                                </Form.Label>
+                            )}
 
+                            <Form.File
+                                id="image-upload-reality"
+                                accept="image/*"
+                                onChange={handleChangeImageReality}
+                            />
                         </Form.Group>
                         <div className="d-md-none">{textFields}</div>
                     </Container>
