ef77de5f17568023834ced97df3f187c3228ab17
Sean Clark <seaniboy2009@yahoo.co.uk>
2023-04-25 08:30:08 +0000

    add component scrolltotop.js

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
+// Each time the component mounts it will scroll to top of page when the location changes
 const ScrollToTop = () => {
   const { pathname } = useLocation();
 
